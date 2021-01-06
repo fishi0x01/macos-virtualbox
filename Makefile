ea9674caeb323076bec1a50912106415a7f7ee33
Karl Fischer <fishi0x01@gmail.com>
2021-01-06 23:00:06 +0100

    Personal config

---
+++ Makefile
@@ -0,0 +1,2 @@
+build-work:
+	 macos_vm_vars_file=configs/work.sh ./macos-guest-virtualbox.sh
---
+++ configs/work.sh
@@ -0,0 +1,8 @@
+export vm_name="OSX"                  # name of the VirtualBox virtual machine
+export macOS_release_name="Catalina"    # install "HighSierra" "Mojave" or "Catalina"
+export storage_size=80000               # VM disk image size in MB, minimum 22000
+export storage_format="vmdk"             # VM disk image file format, "vdi" or "vmdk"
+export cpu_count=4                      # VM CPU cores, minimum 2
+export memory_size=4096                 # VM RAM in MB, minimum 2048
+export gpu_vram=128                     # VM video RAM in MB, minimum 34, maximum 128
+export resolution="1440x900"
